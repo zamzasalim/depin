4f0260c94f24468c3fce7d4059090035ae5b0c6b
Node Army <109174478+sipalingnode@users.noreply.github.com>
2024-11-22 22:37:25 +0700

    Create config.js

---
+++ bless-network/config.js
@@ -0,0 +1,12 @@
+export const Config = [
+  {
+    usertoken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.",
+    nodes: [
+      {
+        nodeId: "12D3KooWNrA....",
+        hardwareId: "f239b6c8b9b4956....",
+        proxy: "noProxy",
+      },
+    ],
+  },
+];
